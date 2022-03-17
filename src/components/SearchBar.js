33aeb0e14d966298d5de6031ea0e9d6573c9f71c
vermasumeet1982 <vermasumeet1982@gmail.com>
2022-03-17 20:11:24 +0000

    converted to arrow function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,10 +2,19 @@ import React from "react";
 class SearchBar extends React.Component {
     state = {term: ''};
 
+    onFormSubmit = event => {
+        event.preventDefault();
+        console.log(this.state.term);
+    }
+        
+    
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form 
+                    onSubmit = {/*(e) => {e.preventDefault();
+                    console.log(this);}*/this.onFormSubmit} 
+                    className="ui form">
                     <div className="field app">
                         <label>Image Search</label>
                         <input 
