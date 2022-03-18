a48690d5383f8e3bd3a69c0f4ae9ceebecf20852
vermasumeet1982 <vermasumeet1982@gmail.com>
2022-03-18 17:12:26 +0000

    some cleanup

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,12 +2,11 @@ import React from "react";
 class ImageList extends React.Component {
 
     render() {
-        const mappedImages = this.props.images.map((i) => {
-            const {
-                id,
-                alt_description,
-                urls,
-            } = i;
+        const mappedImages = this.props.images.map(({
+            id,
+            alt_description,
+            urls,
+        }) => {
             return <img key={id} src={urls?.small} alt={alt_description}/>
         });
         return (mappedImages.length > 0 &&  <div> {mappedImages} </div>)
