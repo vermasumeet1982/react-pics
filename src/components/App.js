52515870128b32db063f5d3cc2e82e64134f3285
vermasumeet1982 <vermasumeet1982@gmail.com>
2022-03-18 15:39:34 +0000

    api request

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import React from "react";
+import axios from 'axios';
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
     
-    onSubmitImageSearch = imageName => {
+    onSubmitImageSearch = async (imageName) => {
         console.log(`Perform search on "${imageName}"`);
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { 
+                query: imageName,
+                per_page: 50,
+            },
+            headers: {
+                Authorization: 'Client-ID BrPdBY-GpVqOufpiPLDoG5Cn3pPLlKWYQylehLWAEWI'
+            }
+        });
+        console.log(response?.data?.results)
     }
 
     render() {
